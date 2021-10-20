d355b99f218f2ca546617b4a3d3b464786767d35
Igor Lapin <raynor73@gmail.com>
2021-10-20 23:59:23 +0100

    Add Game Object

--- GameEngine/game_engine/game_object.cpp
+++ GameEngine/game_engine/game_object.cpp
@@ -1,1 +1,125 @@
 #include "game_object.h"
+#include <sstream>
+
+using namespace GameEngine;
+using namespace std;
+
+GameObject::~GameObject() {
+    for (auto& entry : m_children) {
+        removeChild(entry.second);
+    }
+    for (auto& entry : m_components) {
+        removeComponent(entry.second);
+    }
+}
+
+void GameObject::addChild(const std::shared_ptr<GameObject>& child) {
+    auto childName = child->name();
+    if (m_children.count(childName) > 0) {
+        std::stringstream ss;
+        ss << "Trying to add " << childName << " game object second time";
+        throw std::domain_error(ss.str());
+    }
+
+    m_children[childName] = child;
+    child->m_parent = shared_from_this();
+    child->onAttachedToParent();
+}
+
+void GameObject::removeChild(const std::shared_ptr<GameObject>& child) {
+    auto childName = child->name();
+    if (m_children.count(childName) == 0) {
+        std::stringstream ss;
+        ss << childName << " game object not found";
+        throw std::domain_error(ss.str());
+    }
+
+    m_children.erase(childName);
+    child->m_parent.reset();
+    child->onDetachedFromParent();
+}
+
+void GameObject::addComponent(const std::shared_ptr<GameObjectComponent>& component) {
+    auto componentName = component->typeName();
+    if (m_components.count(componentName) > 0) {
+        std::stringstream ss;
+        ss << "Already have " << componentName << " component";
+        throw std::domain_error(ss.str());
+    }
+
+    m_components[componentName] = component;
+    component->setGameObject(shared_from_this());
+    component->onAttachedToGameObject();
+}
+
+void GameObject::removeComponent(const std::shared_ptr<GameObjectComponent>& component) {
+    auto componentName = component->typeName();
+    if (m_components.count(componentName) == 0) {
+        std::stringstream ss;
+        ss << "Component " << componentName << " not found";
+        throw std::domain_error(ss.str());
+    }
+
+    m_components.erase(componentName);
+    component->setGameObject(nullptr);
+    component->onDetachedFromGameObject();
+}
+
+void GameObject::update() {
+    if (m_isEnabled) {
+        for (auto& entry : m_components) {
+            entry.second->update();
+        }
+
+        for (auto& entry : m_children) {
+            entry.second->update();
+        }
+    }
+}
+
+void GameObject::onAttachedToParent() {
+    for (auto& component : m_components) {
+        component.second->onGameObjectAttachedToParent();
+    }
+}
+
+void GameObject::onDetachedFromParent() {
+    for (auto& component : m_components) {
+        component.second->onGameObjectDetachedFromParent();
+    }
+}
+
+std::shared_ptr<GameObjectComponent> GameObject::findComponent(const std::string& name) {
+    if (m_components.count(name) > 0) {
+        return m_components.at(name);
+    }
+    else {
+        return nullptr;
+    }
+}
+
+std::shared_ptr<GameObject> GameObject::clone() {
+    std::stringstream ss;
+    ss << m_name << nextCloneNamePostfix();
+    return clone(ss.str());
+}
+
+std::shared_ptr<GameObject> GameObject::clone(const std::string& cloneName) {
+    auto clone = GameObject::create(cloneName, m_isEnabled);
+
+    for (auto& entry : m_children) {
+        clone->addChild(entry.second->clone());
+    }
+
+    for (auto& entry : m_components) {
+        clone->addComponent(entry.second->clone());
+    }
+
+    return std::shared_ptr<GameObject>(clone);
+}
+
+int GameObject::s_cloneNamePostfix = 0;
+
+int GameObject::nextCloneNamePostfix() {
+    return s_cloneNamePostfix++;
+}
--- GameEngine/game_engine/game_object.h
+++ GameEngine/game_engine/game_object.h
@@ -7,24 +7,25 @@
 #include <stdexcept>
 #include <sstream>
 #include <game_engine/game_object_component.h>
+#include <game_engine/without_generated_methods.h>
 
 namespace GameEngine
 {
-class GameObject
+class GameObject : public WithoutGeneratedMethods, public std::enable_shared_from_this<GameObject>
 {
     bool m_isEnabled;
     std::string m_name;
 
-    GameObject* m_parent = nullptr;
+    std::weak_ptr<GameObject> m_parent;
     std::unordered_map<std::string, std::shared_ptr<GameObject>> m_children;
     std::unordered_map<std::string, std::shared_ptr<GameObjectComponent>> m_components;
 
     static int s_cloneNamePostfix;
 
 public:
-    explicit GameObject(std::string name);
-    GameObject(const GameObject&) = delete;
-    GameObject(GameObject&&) = delete;
+    static std::shared_ptr<GameObject> create(const std::string &name) { return std::shared_ptr<GameObject>(new GameObject(name, true)); }
+    static std::shared_ptr<GameObject> create(const std::string &name, bool isEnabled) { return std::shared_ptr<GameObject>(new GameObject(name, isEnabled)); }
+
     virtual ~GameObject();
 
     const std::string& name() const { return m_name; }
@@ -32,7 +33,7 @@ public:
     bool isEnabled() const { return m_isEnabled; }
     void setEnabled(bool isEnabled) { m_isEnabled = isEnabled; }
 
-    GameObject* parent() { return m_parent; }
+    std::weak_ptr<GameObject> parent() { return m_parent; }
     const std::unordered_map<std::string, std::shared_ptr<GameObject>>& children() const { return m_children; }
 
     void addChild(const std::shared_ptr<GameObject>& child);
@@ -56,12 +57,12 @@ public:
         }
     }
 
-    GameObject& operator=(const GameObject&) = delete;
-    GameObject& operator=(GameObject&&) = delete;
     std::shared_ptr<GameObject> clone();
     std::shared_ptr<GameObject> clone(const std::string& cloneName);
 
 private:
+    GameObject(std::string name, bool isEnabled) : m_name(std::move(name)), m_isEnabled(true) {}
+
     static int nextCloneNamePostfix();
 };
 }
